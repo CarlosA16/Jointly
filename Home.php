9975978cf73bfe47fd2dae1a5d78e646973a0ea6
CarlosA16 <92894587+CarlosA16@users.noreply.github.com>
2023-03-01 09:26:15 -0500

    Home Pages

---
+++ Home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Home.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jointly</title>
+</head>
+<body>
+    <div id="main">
+        <button>Home</button>
+        <button>Search</button>
+        <button>Explore</button>
+        <h1>Jointly</h1>
+        <button>Notifications</button>
+        <button>Messages</button>
+        <button>Upload</button>
+    </div>
+    
+</body>
+</html>
