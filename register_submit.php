0e131d23900997646b2edabc90f9033f219e2a98
awork211 <work.aaron89@gmail.com>
2023-03-03 10:49:43 -0500

    added db queries

--- register_submit.php
+++ register_submit.php
@@ -3,6 +3,8 @@
 $dbconn = pg_connect("host=localhost dbname=capstone user=aaronwork password=gamecube")
 or die('Could not connect: ' . pg_last_error());
 
+
+
 // Handle form submit
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -10,10 +12,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $str = array($firstName,$lastName,$email,$password);
-    print_r($str);
 
-    
+    $values = array($firstName, $lastName, $email, $password);
+
+    $query = "INSERT INTO users (first_name, last_name, email, password)
+VALUES ($1, $2, $3, $4)";
+
+    $insert_query = pg_query_params($dbconn, $query, $values);
+
+    if(!$insert_query){
+        echo pg_last_error($dbconn);
+    }
 }
 
 pg_close($dbconn);
