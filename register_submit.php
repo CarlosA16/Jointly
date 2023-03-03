211d0b07b6b857af65c979337ddefbf863841704
awork211 <work.aaron89@gmail.com>
2023-03-03 10:02:22 -0500

    updated register

--- register_submit.php
+++ register_submit.php
@@ -1,10 +1,20 @@
 <?php
+// DB connection
+$dbconn = pg_connect("host=localhost dbname=capstone user=aaronwork password=gamecube")
+or die('Could not connect: ' . pg_last_error());
+
+// Handle form submit
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $str = array($firstName,$lastName,$email,$password);
     print_r($str);
+
+    
 }
+
+pg_close($dbconn);
 ?>
