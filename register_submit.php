dc6f58f991a96e1b43445331cd9105d6b16d06b7
awork211 <work.aaron89@gmail.com>
2023-03-03 11:10:45 -0500

    updated register table

--- register_submit.php
+++ register_submit.php
@@ -7,16 +7,16 @@ or die('Could not connect: ' . pg_last_error());
 
 // Handle form submit
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+    $userName = $_POST['userName'];
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $values = array($firstName, $lastName, $email, $password);
+    $values = array($userName, $firstName, $lastName, $email, $password);
 
-    $query = "INSERT INTO users (first_name, last_name, email, password)
-VALUES ($1, $2, $3, $4)";
+    $query = "INSERT INTO users (username, first_name, last_name, email, password)
+VALUES ($1, $2, $3, $4, $5)";
 
     $insert_query = pg_query_params($dbconn, $query, $values);
 
