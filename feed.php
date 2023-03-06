141429eb2d38eae52fbdd5ba9ca69e40bde1926b
CarlosA16 <92894587+CarlosA16@users.noreply.github.com>
2023-03-06 08:55:01 -0500

    Update feed.php

--- feed.php
+++ feed.php
@@ -10,7 +10,7 @@
 <body>
     <div id="header">
         <div id="main">
-            <img style="width:50px; height:50px;margin-left:-40px;margin-right:100px;" src="https://cdn-icons-png.flaticon.com/512/39/39475.png">
+            <img style="width:50px; height:50px;margin-left:-40px;margin-right:100px;margin-top:15px;" src="https://cdn-icons-png.flaticon.com/512/39/39475.png">
         </div>
         <div id="main">
             <button>Home</button>
@@ -27,28 +27,10 @@
     </div>
     <div id="layout">
         <div id="trending">
-            <h1>Trending</h1>
+            <h1 style="margin-right:15px;">Trending</h1>
+            <a href="feed.php" style="text-decoration : underline; color : white;margin-left:-25px;">Thaddeus Stevens College</a>
         </div>
         <div id="feed">
-            <div id="post">
-                <div id="postHeader">
-                    <img src="https://cdn-icons-png.flaticon.com/512/39/39475.png">
-                    <p id="userName">User.Name</p>
-                </div>
-                <img style="margin-top: 4px;margin-bottom: 4px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png">
-                <div id="likes">
-                    <img style="width:30px;height:30px;" src="https://cdn-icons-png.flaticon.com/512/25/25297.png">
-                    <p id="count">189</p>
-                    <p id="count">likes</p>
-                    <img style="width:35px;height:37px;margin-left:35px;margin-top:-2px" src="https://static.thenounproject.com/png/1314304-200.png">
-                    <p id="count">23</p>
-                    <p id="count">comments</p>
-                    <img style="width:30px;height:30px;margin-left:35px;margin-top:3px" src="https://www.pngkey.com/png/full/147-1475657_share-png-share-icon-png.png">
-                    <p id="count">3</p>
-                    <p id="count">shares</p>
-                </div>
-                <p id="desc">default description of image, image descriptions have to be short, they may not be more than 2 lines</p>
-            </div>
             <div id="post">
                 <div id="postHeader">
                     <img src="https://cdn-icons-png.flaticon.com/512/39/39475.png">
