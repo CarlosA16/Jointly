fe8806a90228fd305552de3921cba1e9bc21b85f
awork211 <work.aaron89@gmail.com>
2023-04-07 11:07:38 -0400

    user img retrieval script added

---
+++ get_img.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'db_conn.php';
+
+$query = "SELECT profile_image FROM users WHERE username = $1";
+$result = pg_query_params($dbconn, $query, array($_SESSION['active_user']));
+
+if ($result) {
+    $image_bytea = pg_fetch_result($result, 0, 'profile_image');
+
+    // Convert the bytea data to a string
+    $image_string = pg_unescape_bytea($image_bytea);
+
+    $img_type = image_type_to_mime_type($image_string);
+    
+    header('Content-Type: '. $img_type);
+
+    
+    echo $image_string;
+} else {
+
+    echo "Error executing query: " . pg_last_error($dbconn);
+}
+
+pg_close($dbconn);
+?>
