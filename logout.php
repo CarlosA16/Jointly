a459642413feed73668a90e8b40d13368d604828
awork211 <work.aaron89@gmail.com>
2023-04-07 11:07:04 -0400

    changed action link

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 // prevent unlogged users from viewing feed
-// session_start();
+session_start();
 
 // if(empty($_SESSION['username'])){
 //     $_SESSION["flash"] = ["type" => "warning", "message" => "Must be logged in."];
